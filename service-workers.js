4e0d8eb0005fd9ae446c407b3d5588a7eef3c875
bartoshevich <25452644+bartoshevich@users.noreply.github.com>
2020-05-06 09:38:23 +0300

    2

--- service-workers.js
+++ service-workers.js
@@ -17,11 +17,7 @@ var urlsToCache = [
   '/uslugi/',
   '/uslugi/brand-conception/',
   '/opyt/brand-giperlink/',
-  '/opyt/brand-strategy-flex-n-roll/',
-  '/images/me2_s_520.webp',
-  '/images/me2_s_700.webp',
-  '/images/me2-620.webp',
-  '/images/me2-840.webp'
+  '/opyt/brand-strategy-flex-n-roll/'
  ];
 
 
